1be8f5751f3a9597293a5358630dd482c6cde116
Ian Moon <55413858+Quarter-Luna@users.noreply.github.com>
2022-04-20 21:17:18 -0600

    print

--- wyshell.c
+++ wyshell.c
@@ -23,7 +23,7 @@ int main()
         parse_line(fgets(args, 4096, stdin));
         if(args != NULL)
         {
-            fprint("args filled");
+            printf("args filled");
         }
     }
 }
