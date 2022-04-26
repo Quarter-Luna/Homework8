d28741031281d6e51ccbdc112acc5bc7445ee325
Ian Moon <55413858+Quarter-Luna@users.noreply.github.com>
2022-04-26 14:19:36 -0600

    this

--- wyshell.c
+++ wyshell.c
@@ -29,19 +29,19 @@
 
 struct word
 {
-    struct word *next, *prev;
-    char *string;
+  struct word *next, *prev;
+  char *string;
 };
 
 typedef struct word Word;
 
 struct node
 {
-    struct node *node, *prev;
-    char *command;
-    Word *arg_list;
-    int input, output, error;
-    char *in_file, *out_file, *err_file;
+  struct node *node, *prev;
+  char *command;
+  Word *arg_list;
+  int input, output, error;
+  char *in_file, *out_file, *err_file;
 };
 
 typedef struct node Node;
@@ -52,79 +52,79 @@ char buff[4096];
 
 void addToList(char *input, Node *list)
 {
-    Word *tmp;
-    tmp = list->arg_list;
-    if (tmp == NULL)
-    {
-        tmp = calloc(1, sizeof(Word));
-        tmp->string = strdup(input);
-        list->arg_list = tmp;
-    }
-    else
-    {
-        while (tmp->next != NULL)
-            tmp = tmp->next;
-        tmp->next = calloc(1, sizeof(Word));
-        tmp->next->prev = tmp;
-        tmp->next->string = strdup(input);
-    }
+  Word *tmp;
+  tmp = list->arg_list;
+  if (tmp == NULL)
+  {
+    tmp = calloc(1, sizeof(Word));
+    tmp->string = strdup(input);
     list->arg_list = tmp;
+  }
+  else
+  {
+    while (tmp->next != NULL)
+      tmp = tmp->next;
+    tmp->next = calloc(1, sizeof(Word));
+    tmp->next->prev = tmp;
+    tmp->next->string = strdup(input);
+  }
+  list->arg_list = tmp;
 }
 
 int main()
 {
-    while (1)
+  while (1)
+  {
+    printf("$> ");
+    prtn = fgets(buff, 4096, stdin);
+    Node *Head, *current = NULL;
+    Word *commands = NULL;
+    rtn = parse_line(buff);
+    current = calloc(1, sizeof(Node));
+    while (rtn != EOL)
     {
-        printf("$> ");
-        prtn = fgets(buff, 4096, stdin);
-        Node *Head, *current = NULL;
-        Word *commands = NULL;
-        rtn = parse_line(buff);
-        current = calloc(1, sizeof(Node));
-        while (rtn != EOL) 
+      switch (rtn)
+      {
+      case WORD:
+        if (Head == NULL)
         {
-            switch (rtn)
-            {
-            case WORD:
-                if (Head == NULL)
-                {
-                    Head = calloc(1, sizeof(Node));
-                    current = Head;
-                    // printf("head created");
-                }
-                
+          Head = calloc(1, sizeof(Node));
+          current = Head;
+          // printf("head created");
+        }
 
-                if (current->command == NULL)
-                {
-                    current->command = strdup(lexeme);
-                    printf(":--: %s\n", lexeme);
-                    // printf("String duplicated");
-                }
-                else
-                {
-                    addToList(lexeme, current);
-                    printf(" --: %s\n", lexeme);
-                    // printf("Command added to list");
-                }
-                rtn = parse_line(rtn);
-                // commands = head;
-            // case REDIR_OUT:
-            //     printf(">\n");
-            // }
-            default:
-              printf("argument invalid");
-            }
-        }
-        commands = calloc(1, sizeof(Word));
-        commands = current->arg_list;
-        /*while(commands->prev != NULL){
-            commands = commands->prev;
-        }
-        while (commands != NULL)
+        if (current->command == NULL)
         {
-            printf("%s\n", commands->string);
-            commands = commands->next;
+          current->command = strdup(lexeme);
+          printf(":--: %s\n", lexeme);
+          // printf("String duplicated");
         }
-        */
+        else
+        {
+          addToList(lexeme, current);
+          printf(" --: %s\n", lexeme);
+          // printf("Command added to list");
+        }
+        
+        // commands = head;
+      // case REDIR_OUT:
+      //     printf(">\n");
+      // }
+      default:
+        printf("argument invalid");
+      }
+      parse_line(NULL);
     }
+    commands = calloc(1, sizeof(Word));
+    commands = current->arg_list;
+    /*while(commands->prev != NULL){
+        commands = commands->prev;
+    }
+    while (commands != NULL)
+    {
+        printf("%s\n", commands->string);
+        commands = commands->next;
+    }
+    */
+  }
 }
